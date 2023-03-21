141beeaacff951f7e8742f4bfa2ec12bffa2c938
Ann KangHo <102593699+gangho0922@users.noreply.github.com>
2023-03-21 23:08:12 +0900

    Initial Commit

---
+++ CollectionView_NoStoryboard/UICollectionViewController.swift
@@ -0,0 +1,16 @@
+//
+//  UICollectionViewController.swift
+//  CollectionView_NoStoryboard
+//
+//  Created by AnnKangHo on 2023/03/21.
+//
+
+import UIKit
+
+class UICollectionViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            
+    }
+}
---
+++ CollectionView_NoStoryboard/WholeCell.swift
@@ -0,0 +1,12 @@
+//
+//  WholeCell.swift
+//  CollectionView_NoStoryboard
+//
+//  Created by AnnKangHo on 2023/03/21.
+//
+
+import UIKit
+
+class WholeCell: UICollectionViewCell {
+    
+}
