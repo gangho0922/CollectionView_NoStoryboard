d37f22f6c30659b29d5b94d0dc2b49f8f54e967f
Ann KangHo <102593699+gangho0922@users.noreply.github.com>
2023-03-21 23:39:12 +0900

    CollectionView 간격 설정 및 사용 방법

--- CollectionView_NoStoryboard/ViewController.swift
+++ CollectionView_NoStoryboard/ViewController.swift
@@ -1,1 +1,75 @@
+//
+//  ViewController.swift
+//  CollectionView_NoStoryboard
+//
+//  Created by AnnKangHo on 2023/03/21.
+//
 
+import UIKit
+
+let cellID = "Cell"
+
+class ViewController: UIViewController {
+
+    
+    let collectionView: UICollectionView = {
+        let flowlayout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: flowlayout)
+        return cv
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        view.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        collectionView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        collectionView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        //제약 설정
+        collectionView.register(WholeCell.self, forCellWithReuseIdentifier: cellID)
+        //cell 만들려면 register 사용
+        view.backgroundColor = .white
+        collectionView.backgroundColor = .systemBrown
+    }
+
+}
+extension ViewController: UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath) as! WholeCell
+        
+        cell.backgroundColor = .black
+        
+        
+        return cell
+    }
+}
+extension ViewController: UICollectionViewDelegate {
+    
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        
+        return CGSize(width: (view.frame.width - 40) / 4, height: (view.frame.width - 40) / 4)
+    }
+    // 셀 사이즈 설정
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    // 행 하나에 셀과 셀 사이에 간격 두기
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+    }
+    //위와 cell의 간격 주기
+}
